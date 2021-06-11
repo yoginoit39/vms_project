655f098f4efcc125b05cf8ba3bc63fcd27fbd344
yoginoit39 <ylakhani39@gmail.com>
2021-06-11 16:59:11 +0300

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<div class="row">
+                            <div class="col-md-12">
+                                <div class="copyright">
+                                    <p>@2021 Final Year Project </p>
+                                </div>
+                            </div>
+                        </div>
